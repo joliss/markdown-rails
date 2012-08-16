9bc90f2626289a633167dbe4a3bf9882ae10823f
Bill Transue <transue@gmail.com>
2012-08-16 16:28:13 -0400

    Add Compatibility for All Rails Versions
    Work though the multitude of classes within ActionView where the
    register_template_handler has appeared over the years.

--- lib/markdown-rails/engine.rb
+++ lib/markdown-rails/engine.rb
@@ -33,5 +33,22 @@ MarkdownRails.configure do |config|
 end
 
 handler = MarkdownRails::Handler.new
-ActionView::Template.register_template_handler(:md, handler)
-ActionView::Template.register_template_handler(:markdown, handler)
+
+[:md, :markdown].each do |extension|
+  # >= v3.0.5
+  if defined? ActionView::Template::Handlers and ActionView::Template::Handlers.respond_to? :register_template_handler
+    ActionView::Template::Handlers
+  # >= v2.1.0 <= v2.1.0
+  elsif defined? ActionView::Template and ActionView::Template.respond_to? :register_template_handler
+    ActionView::Template
+  # >= v2.2.1 <= v2.3.8
+  elsif defined? ActionView::TemplateHandlers and ActionView::TemplateHandlers.respond_to? :register_template_handler
+    ActionView::TemplateHandlers
+  # <= v2.0.3
+  elsif defined? ActionView::Base and ActionView::Base.respond_to? :register_template_handler
+    ActionView::Base
+  # I give up...
+  else
+    raise "Couldn't find `register_template_handler' method in ActionView module."
+  end.register_template_handler extension, handler
+end
